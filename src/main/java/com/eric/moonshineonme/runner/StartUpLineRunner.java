816bb834a966dadb7ce913032790593f1212fe8c
eric <cxywf9337@163.com>
2020-07-09 11:00:46 +0800

    for test jenkins

--- src/main/java/com/eric/moonshineonme/runner/StartUpLineRunner.java
+++ src/main/java/com/eric/moonshineonme/runner/StartUpLineRunner.java
@@ -23,7 +23,7 @@ public class StartUpLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         while (true) {
-            log.info("My project moon-shine-on-me is running 3.");
+            log.info("My project moon-shine-on-me is running 4.");
             Thread.sleep(10 * 1000L);
         }
     }
